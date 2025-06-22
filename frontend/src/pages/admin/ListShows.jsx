b1cbf21ad8b4f0e92d5293814b253a3882007e17
Krishna Sikheriya <krishnasikheriya001@gmail.com>
2025-06-22 22:16:58 +0530

    completed the fontend of the webpage

--- frontend/src/pages/admin/ListShows.jsx
+++ frontend/src/pages/admin/ListShows.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { dummyShowsData } from "../../assets/assets";
 import Loading from "../../components/Loading";
 import Title from "../../components/admin/Title";
+import { dateFormat } from "../../lib/dateFormat";
 
 const ListShows = () => {
 	const currency = import.meta.env.VITE_CURRENCY;
@@ -13,7 +14,7 @@ const ListShows = () => {
 		setShows([
 			{
 				movie: dummyShowsData[0],
-				showDateTime: "2025-06-30T02:30:00:002",
+				showDateTime: "2025-06-30T02:30:00.002",
 				showPrice: 59,
 				occupiedSeats: {
 					A1: "user_1",
@@ -42,6 +43,18 @@ const ListShows = () => {
               <th className="p-2 font-medium">Earnings</th>
             </tr>
           </thead>
+          <tbody className="text-sm font-light">
+            {
+              shows.map((show, index) => (
+                <tr key={index} className="border-b border-primary/10 bg-primary/5 even:bg-primary/10">
+                  <td className="p-2 min-w-45 pl-5">{show.movie.title}</td>
+                  <td className="p-2">{dateFormat(show.showDateTime)}</td>
+                  <td className="p-2">{Object.keys(show.occupiedSeats).length}</td>
+                  <td className="p-2">{currency} {Object.keys(show.occupiedSeats).length * show.showPrice}</td>
+                </tr>
+              ))
+            }
+          </tbody>
         </table>
       </div>
 		</>
