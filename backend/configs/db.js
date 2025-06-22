aabc5976ea86105e50b55888455f59474d2a520e
Krishna Sikheriya <krishnasikheriya001@gmail.com>
2025-06-22 23:03:54 +0530

    setup backend, clerk, inngest before deploying

---
+++ backend/configs/db.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+	const databaseName = "quickshow";
+	try {
+		mongoose.connection.on("connected", () => console.log("MongoDB Connected"));
+		await mongoose.connect(`${process.env.MONGOB_URI}/${databaseName}`);
+	} catch (error) {
+		console.log(error.message);
+	}
+};
+
+export default connectDB;
